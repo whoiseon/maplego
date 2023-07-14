ffac8fe8710990221e8fe96d86f23828c21f78bc
whoiseon <whois__@naver.com>
2023-07-14 14:31:33 +0900

    feat: initialized web & server

---
+++ packages/web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className='h-screen w-full bg-white dark:bg-gray-900'>
+      <h1 className='text-3xl text-pink-500'>MS-IN 오픈 준비중</h1>
+    </main>
+  )
+}
---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        pretendard: ['Pretendard', 'ui-sans-serif', 'sans-serif', 'system-ui'],
+      }
+    },
+  },
+  plugins: [],
+}
