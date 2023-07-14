20b1388de7948329691d24fb61842e0146785b00
whoiseon <whois__@naver.com>
2023-07-14 18:00:17 +0900

    feat: Implement parsing to get game rankings and character info

--- packages/server/src/routes/api/test/index.ts
+++ packages/server/src/routes/api/test/index.ts
@@ -1,10 +1,31 @@
 import Router from '@koa/router';
+import crawler, { RankingType } from 'src/lib/crawler';
 
 const test = new Router();
 
-test.get('/ping', async ctx => {
+// example http://localhost:3060/api/test/rank?type=Total&page=1&world=0
+test.get('/rank', async ctx => {
+  const { type, page, world } = ctx.query;
+  const list = await crawler.getRanking({
+    type: type as RankingType,
+    page: page as string,
+    world: world as string,
+  });
+
   ctx.body = {
     message: 'pong',
+    list,
+  };
+});
+
+// example http://localhost:3060/api/test/character?nickname=캐릭터명
+test.get('/character', async ctx => {
+  const { nickname } = ctx.query;
+  const character = await crawler.getCharacterInfo(nickname as string);
+
+  ctx.body = {
+    message: 'pong',
+    data: character,
   };
 });
 
