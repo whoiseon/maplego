71808eadbde33546ed18d8c2ec84e2de9e70cfd0
whoiseon <whois__@naver.com>
2023-07-19 20:52:25 +0900

    feat: initialized nestjs

--- packages/server/src/app.ts
+++
@@ -1,5 +0,0 @@
-import Server from './server';
-
-/** server start */
-const server = new Server();
-server.start();
--- packages/server/src/routes/api/index.ts
+++
@@ -1,7 +0,0 @@
-import Router from '@koa/router';
-import test from './test';
-
-const api = new Router();
-api.use('/test', test.routes());
-
-export default api;
